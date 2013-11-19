75df52b3966186ebdb56aba654dc9236714819bd
David Radcliffe <radcliffe.david@gmail.com>
2013-11-19 09:48:22 -0500

    bump solr version and url (old mirrors are gone?)

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,6 @@
 # Copyright 2013, David Radcliffe
 #
 
-default['solr']['version']  = '4.4.0'
-default['solr']['url']      = "http://mirror.cogentco.com/pub/apache/lucene/solr/#{node['solr']['version']}/solr-#{node['solr']['version']}.tgz"
+default['solr']['version']  = '4.5.1'
+default['solr']['url']      = "http://apache.mirrors.lucidnetworks.net/lucene/solr/#{node['solr']['version']}/solr-#{node['solr']['version']}.tgz"
 default['solr']['data_dir'] = '/etc/solr'
