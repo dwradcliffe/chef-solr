ca77648892b3cf5744f2902bbe9e28cf36b829dc
David Radcliffe <radcliffe.david@gmail.com>
2014-02-05 11:50:43 -0500

    bump to version 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'radcliffe.david@gmail.com'
 license          'MIT'
 description      'Installs the solr search engine.'
 long_description 'See README.md'
-version          '0.1.0'
+version          '0.1.2'
 
 supports 'redhat'
 supports 'centos'
