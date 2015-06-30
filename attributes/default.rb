9dc4c14f6bd988e4d83fbc7579672e09258476a2
Eric Tucker <eric.tucker@bluespurs.com>
2015-06-30 09:56:31 -0300

    Bump solr version to last 4.x.  Change default port to match "normal" default.

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,11 +5,11 @@
 # Copyright 2013, David Radcliffe
 #
 
-default['solr']['version']  = '4.6.1'
+default['solr']['version']  = '4.10.4'
 default['solr']['url']      = "https://archive.apache.org/dist/lucene/solr/#{node['solr']['version']}/solr-#{node['solr']['version']}.tgz"
 default['solr']['data_dir'] = '/etc/solr'
 default['solr']['dir']      = '/opt/solr'
-default['solr']['port']     = '8984'
+default['solr']['port']     = '8983'
 default['solr']['pid_file'] = '/var/run/solr.pid'
 default['solr']['log_file'] = '/var/log/solr.log'
 default['solr']['user']     = 'root'
