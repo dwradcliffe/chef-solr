f628c8b05d9e8e2a0660f1b49f199b6556529c07
David Radcliffe <radcliffe.david@gmail.com>
2015-07-23 22:12:35 -0400

    bump version to 0.5.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'radcliffe.david@gmail.com'
 license          'MIT'
 description      'Installs the solr search engine.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '0.5.0'
 
 supports 'amazon'
 supports 'centos'
